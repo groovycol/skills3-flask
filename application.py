b9aa375957327c3fa7580076a6040c7f24137105
Colleen <groovycol@gmail.com>
2016-04-24 13:25:46 -0700

    Added index.html and cleaned up css

--- application.py
+++ application.py
@@ -6,13 +6,8 @@ app = Flask(__name__)
 @app.route("/")
 def index_page():
     """Show an index page."""
-
-    return "<html><body>This is the homepage.</body></html>"
-
-    # Alternately, we could make this a Jinja template in `templates/`
-    # and return that result of rendering this, like:
-    #
-    # return render_template("index.html")
+  
+    return render_template("index.html")
 
 
 @app.route("/application-form")
